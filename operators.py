746d1eec2e7c33e9b71f312b2b21c081af3cea9e
ShlokThandole <tgrao940@gmail.com>
2024-10-12 22:10:59 +0530

    add dos

---
+++ operators.py
@@ -0,0 +1,21 @@
+num1 = 74
+num2 = 9
+
+print("number 1:", num1)
+print("number 2:", num2)
+print("addition:", num1+num2)
+print("difference:", num1-num2)
+print("product:", num1*num2)
+print("division:", num1/num2)
+print("floor division:", num1//num2)
+print("modulus operation:", num1%num2)
+print("square:", num1**2)
+print("square root:", num2**0.5)
+
+print("equal ?", num1==num2)
+print("number 1 greater ?", num1>num2)
+print("number 2 greater ?", num1<num2)
+print("not equal ?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("result of the equation =", result)
---
+++ swap.py
@@ -0,0 +1,12 @@
+#take input values from the user
+x = input("enter value of x :")
+y = input("enter value of y :")
+
+#swapping
+temp = x
+x = y
+y = temp
+
+#results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
